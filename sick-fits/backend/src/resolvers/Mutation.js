bb65004b60e6d63cfdb5dd83ee25bc2ea1e87576
Arthur Denner <arthurdenner7@gmail.com>
2019-01-20 18:27:33 -0300

    feat(backend): Adding a GraphQL Yoga server and playing with it a little

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const mutations = {};
+const mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    global.dogs.push(args);
+
+    return args;
+  },
+};
 
 module.exports = mutations;
