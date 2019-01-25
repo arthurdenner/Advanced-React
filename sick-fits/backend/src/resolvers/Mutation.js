9b267df8f31a78fec11f2d6946297dc67078df5d
Arthur Denner <arthurdenner7@gmail.com>
2019-01-24 23:24:32 -0300

    feat(backend): Adding mutation to delete an item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,8 +1,13 @@
 const mutations = {
-  async createItem(parent, args, ctx, info) {
+  createItem(parent, args, ctx, info) {
     // TODO: Check if the user is logged in
 
     return ctx.db.mutation.createItem({ data: { ...args } }, info);
+  },
+  deleteItem(parent, args, ctx, info) {
+    const { id } = args;
+
+    return ctx.db.mutation.deleteItem({ where: { id } }, info);
   },
   updateItem(parent, args, ctx, info) {
     const { id, ...updates } = args;
