bb901efde32cba98ae0f93bf91c1ed00931d3e2c
Arthur Denner <arthurdenner7@gmail.com>
2019-01-26 17:16:49 -0300

    feat(backend): Adding mutation to signup

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,3 +1,5 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config();
 
 const createServer = require('./createServer');
@@ -5,7 +7,7 @@ const db = require('./db');
 
 const server = createServer();
 
-// TODO: Use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 // TODO: Use express middleware to populate current user
 
 server.start(
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const mutations = {
   createItem(parent, args, ctx, info) {
     // TODO: Check if the user is logged in
@@ -13,6 +16,28 @@ const mutations = {
     const { id, ...updates } = args;
 
     return ctx.db.mutation.updateItem({ data: updates, where: { id } }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          email: args.email.toLowerCase(),
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+
+    return user;
   },
 };
 
