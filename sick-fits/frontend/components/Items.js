6af16a3f72e4aaef07c0fbc11f3d88ad9a8d4041
Arthur Denner <arthurdenner7@gmail.com>
2019-01-26 14:44:40 -0300

    feat: Making pagination works :tada:

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -7,8 +7,8 @@ import Item from './Item';
 import Pagination from './Pagination';
 
 export const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -38,7 +38,12 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: page * perPage - perPage,
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
