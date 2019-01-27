5f52d3f7030b509e3c7ba09904723b01684bad7c
Arthur Denner <arthurdenner7@gmail.com>
2019-01-27 16:33:50 -0300

    feat(frontend): Creating component to handle addition of cart items

---
+++ sick-fits/frontend/components/AddToCart.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const ADD_TO_CART_MUTATION = gql`
+  mutation ADD_TO_CART_MUTATION($id: ID!) {
+    addToCart(id: $id) {
+      id
+      quantity
+    }
+  }
+`;
+
+const AddToCart = ({ id }) => (
+  <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
+    {addToCart => (
+      <button
+        onClick={() => {
+          addToCart().catch(err => {
+            alert(err.message);
+          });
+        }}
+      >
+        Add To Cart 🛒
+      </button>
+    )}
+  </Mutation>
+);
+
+export default AddToCart;
