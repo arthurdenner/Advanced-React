a420e32f57a3b1e6dd6ded70b6d4782652661118
Arthur Denner <arthurdenner7@gmail.com>
2019-01-27 17:22:07 -0300

    feat(frontend): Showing the cart items and total

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation ADD_TO_CART_MUTATION($id: ID!) {
@@ -12,16 +13,21 @@ const ADD_TO_CART_MUTATION = gql`
 `;
 
 const AddToCart = ({ id }) => (
-  <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
-    {addToCart => (
+  <Mutation
+    mutation={ADD_TO_CART_MUTATION}
+    variables={{ id }}
+    refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+  >
+    {(addToCart, { loading }) => (
       <button
+        disabled={loading}
         onClick={() => {
           addToCart().catch(err => {
             alert(err.message);
           });
         }}
       >
-        Add To Cart 🛒
+        Add{loading && 'ing'} to Cart 🛒
       </button>
     )}
   </Mutation>
