cb00ca600d0b52b70adad5a33b5beda5402d5812
Arthur Denner <arthurdenner7@gmail.com>
2019-01-27 17:34:52 -0300

    feat(backend): Implementing mutation to remove item from cart

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -204,6 +204,24 @@ const mutations = {
       info
     );
   },
+  async removeFromCart(parent, args, ctx, info) {
+    checkLogin(ctx);
+
+    const cartItem = await ctx.db.query.cartItem(
+      { where: { id: args.id } },
+      `{ id, user { id }}`
+    );
+
+    if (!cartItem) {
+      throw new Error('No such cart item found');
+    }
+
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error("You don't own this item");
+    }
+
+    return ctx.db.mutation.deleteCartItem({ where: { id: args.id } }, info);
+  },
 };
 
 module.exports = mutations;
