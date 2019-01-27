3e2ddbb7dfcd109e35f78dd7469d99c8e667c588
Arthur Denner <arthurdenner7@gmail.com>
2019-01-26 22:57:22 -0300

    feat(backend): Implementing signin and signout flows

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,6 +1,16 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const setCookie = (ctx, userId) => {
+  // 1. generate the JWT Token
+  const token = jwt.sign({ userId }, process.env.APP_SECRET);
+  // 2. Set the cookie with the token
+  ctx.response.cookie('token', token, {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+  });
+};
+
 const mutations = {
   createItem(parent, args, ctx, info) {
     // TODO: Check if the user is logged in
@@ -30,14 +40,34 @@ const mutations = {
       },
       info
     );
-    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
 
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-    });
+    setCookie(ctx, user.id);
 
     return user;
+  },
+
+  async signin(parent, { email, password }, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email } });
+
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+
+    // 2. Check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('Invalid Password!');
+    }
+
+    setCookie(ctx, user.id);
+
+    return user;
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+
+    return { message: 'Goodbye!' };
   },
 };
 
