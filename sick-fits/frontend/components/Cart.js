7fd1f796dbafe9d692ae64d20976e8ac5171e08d
Arthur Denner <arthurdenner7@gmail.com>
2019-01-28 19:37:43 -0300

    refactor(frontend): Removing nested render props with react-adopt

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Mutation, Query } from 'react-apollo';
+import { adopt } from 'react-adopt';
 import gql from 'graphql-tag';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import formatMoney from '../lib/formatMoney';
@@ -22,9 +23,21 @@ const TOGGLE_CART_MUTATION = gql`
   }
 `;
 
+/* eslint-disable */
+const Composed = adopt({
+  user: ({ render }) => <User>{render}</User>,
+  toggleCart: ({ render }) => (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+  ),
+  localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
+});
+/* eslint-enable */
+
 const Cart = () => (
-  <User>
-    {({ data: { me } }) => {
+  <Composed>
+    {({ user, toggleCart, localState }) => {
+      const me = user.data.me;
+
       if (!me) {
         return null;
       }
@@ -32,38 +45,30 @@ const Cart = () => (
       const validItemsCount = me.cart.filter(cartItem => cartItem.item).length;
 
       return (
-        <Mutation mutation={TOGGLE_CART_MUTATION}>
-          {toggleCart => (
-            <Query query={LOCAL_STATE_QUERY}>
-              {({ data }) => (
-                <CartStyles open={data.cartOpen}>
-                  <header>
-                    <CloseButton onClick={toggleCart} title="close">
-                      &times;
-                    </CloseButton>
-                    <Supreme>{me.name}'s Cart</Supreme>
-                    <p>
-                      You have {validItemsCount} item
-                      {validItemsCount === 1 ? '' : 's'} in your cart.
-                    </p>
-                  </header>
-                  <ul>
-                    {me.cart.map(cartItem => (
-                      <CartItem key={cartItem.id} cartItem={cartItem} />
-                    ))}
-                  </ul>
-                  <footer>
-                    <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                    <SickButton>Checkout</SickButton>
-                  </footer>
-                </CartStyles>
-              )}
-            </Query>
-          )}
-        </Mutation>
+        <CartStyles open={localState.data.cartOpen}>
+          <header>
+            <CloseButton onClick={toggleCart} title="close">
+              &times;
+            </CloseButton>
+            <Supreme>{me.name}'s Cart</Supreme>
+            <p>
+              You have {validItemsCount} item
+              {validItemsCount === 1 ? '' : 's'} in your cart.
+            </p>
+          </header>
+          <ul>
+            {me.cart.map(cartItem => (
+              <CartItem key={cartItem.id} cartItem={cartItem} />
+            ))}
+          </ul>
+          <footer>
+            <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+            <SickButton>Checkout</SickButton>
+          </footer>
+        </CartStyles>
       );
     }}
-  </User>
+  </Composed>
 );
 
 export default Cart;
--- sick-fits/frontend/lib/calcTotalItems.js
+++ sick-fits/frontend/lib/calcTotalItems.js
@@ -1,3 +1,5 @@
-export default function calcTotalItems(cart) {
-  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
+export default function lccalcTotalItems(cart) {
+  return cart
+    .filter(cartItem => cartItem.item)
+    .reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 }
