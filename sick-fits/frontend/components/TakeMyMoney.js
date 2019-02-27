3ac1b0f9687b2d73391346ad499f5649c80899d2
Arthur Denner <arthurdenner7@gmail.com>
2019-02-27 20:59:30 -0300

    feat(frontend): Charge a credit card in the server and create an order

--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -10,31 +10,59 @@ import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
 
-class TakeMyMoney extends React.Component {
-  onToken = res => {
-    console.log('On Token Called!');
-    console.log(res.id);
-  };
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
 
+class TakeMyMoney extends React.Component {
   render() {
     const { children } = this.props;
 
     return (
       <User>
-        {({ data: { me } }) => (
-          <StripeCheckout
-            amount={calcTotalPrice(me.cart)}
-            name="Sick Fits"
-            description={`Order of ${calcTotalItems(me.cart)} items!`}
-            image={me.cart[0].item && me.cart[0].item.image}
-            stripeKey="pk_test_Vtknn6vSdcZWSG2JWvEiWSqC"
-            currency="USD"
-            email={me.email}
-            token={res => this.onToken(res)}
-          >
-            {children}
-          </StripeCheckout>
-        )}
+        {({ data: { me } }) =>
+          me.cart.length > 0 && (
+            <Mutation
+              mutation={CREATE_ORDER_MUTATION}
+              refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+            >
+              {createOrder => (
+                <StripeCheckout
+                  amount={calcTotalPrice(me.cart)}
+                  name="Sick Fits"
+                  description={`Order of ${calcTotalItems(me.cart)} items!`}
+                  image={me.cart[0].item && me.cart[0].item.image}
+                  stripeKey="pk_test_w7i4UODoGG4Vm3AvBcrF1GW3"
+                  currency="USD"
+                  email={me.email}
+                  token={async res => {
+                    try {
+                      const order = await createOrder({
+                        variables: { token: res.id },
+                      });
+
+                      console.log(order);
+                    } catch (err) {
+                      alert(err.message);
+                    }
+                  }}
+                >
+                  {children}
+                </StripeCheckout>
+              )}
+            </Mutation>
+          )
+        }
       </User>
     );
   }
